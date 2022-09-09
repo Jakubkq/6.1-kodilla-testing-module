449bf69a68093ad29c5d2a5f3d638820a60388ea
Jakub Ciepowicz <jakub.cieploiwcz@wp.pl>
2022-09-09 19:02:40 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
+
+
+import java.util.Scanner;
 
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        double a = 10;
+        double b = 5;
+
+        double result = calculator.Add(a, b);
+        if (result == 15) {
+            System.out.println("test zaliczony");
+        }
+        else {
+            System.out.println("test niezaliczony");
+        }
+
+        double result1  = calculator.substract(a , b);
+        if (result1 == 5) {
+            System.out.println("test zaliczony");
+        }
+        else {
+            System.out.println("test niezaliczony");
+        }
+        }
     }
-}
+
+
+
+

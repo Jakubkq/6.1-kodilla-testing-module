1cab917112aafc3911f17a3eadcf3aaad0a3fa51
Jakub Ciepowicz <jakub.cieploiwcz@wp.pl>
2022-09-11 18:15:05 +0200

    6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,31 +1,16 @@
 package com.kodilla.testing;
-import com.kodilla.testing.calculator.Calculator;
 
 
-import java.util.Scanner;
+
+
 
 public class TestingMain {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        double a = 10;
-        double b = 5;
 
-        double result = calculator.Add(a, b);
-        if (result == 15) {
-            System.out.println("test zaliczony");
-        }
-        else {
-            System.out.println("test niezaliczony");
-        }
 
-        double result1  = calculator.substract(a , b);
-        if (result1 == 5) {
-            System.out.println("test zaliczony");
-        }
-        else {
-            System.out.println("test niezaliczony");
-        }
+
+
         }
     }
 
